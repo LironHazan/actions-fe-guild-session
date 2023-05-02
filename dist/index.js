0ad97ff3fd47b18b8645272432dbd458aff15cee
lironh <lironenk@gmail.com>
2023-05-02 21:52:06 +0300

    updated action

--- dist/index.js
+++ dist/index.js
@@ -16,24 +16,29 @@ async function runCowsayCli(what) {
     });
     return output;
 }
+const COMMENT_ID = 'Your code is moognificant';
 async function getComment(octokit) {
-    var _a, _b;
+    var _a;
     // Check if a comment already exists for this PR from the bot
     const commentsResult = await octokit.rest.issues.listComments({
         repo: github_1.context.repo.repo,
         owner: github_1.context.repo.owner,
-        issue_number: (_b = (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.number) !== null && _b !== void 0 ? _b : 0,
+        issue_number: (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.number,
     });
-    return commentsResult.data.find(comment => { var _a; return ((_a = comment.user) === null || _a === void 0 ? void 0 : _a.login) === github_1.context.actor; });
+    return commentsResult.data.find(comment => { var _a; return (_a = comment.body) === null || _a === void 0 ? void 0 : _a.includes(COMMENT_ID); });
 }
 async function run() {
+    var _a;
     if (!ghToken || !github_1.context.payload.pull_request)
         return;
     const octokit = (0, github_1.getOctokit)(ghToken);
+    console.log('Pr_number', (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.number);
     const prevComment = await getComment(octokit);
-    let output = await runCowsayCli('Your code is moognificant  ☀️');
+    let output = await runCowsayCli(`${COMMENT_ID} ☀️`);
     const commentBody = String(output).trim();
-    prevComment ? await octokit.rest.issues.updateComment({
+    console.log(!!prevComment);
+    console.log(prevComment.id);
+    !!prevComment ? await octokit.rest.issues.updateComment({
         repo: github_1.context.repo.repo,
         owner: github_1.context.repo.owner,
         comment_id: prevComment.id,
