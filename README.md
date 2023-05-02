77baa5ec8516c5061de25754fa004ce0ee98f3ef
LironH <lironenk@gmail.com>
2023-05-02 22:32:09 +0300

    packing all (#6)
    * packing all
    
    * packing all

--- README.md
+++ README.md
@@ -1,52 +1,47 @@
-
 DIY it's easy - GHA in Typescript
 
-----
+---
+
 As part of js fatigue sessions aimed for the FE/FS Guild (@SentinelOne)
 
 ---
-**Agenda:** 
+
+**Agenda:**
 
 - Briefly go over on core concepts
 - Briefly go over types of custom actions
   - Example of a "composite" action
 - Using `actions/github-script@v6`
-- Creating own action in typescript! 
+- Creating own action in typescript!
 
------
+---
 
 **Composite**
 
 Example of the composite action is located under [`.github/actions/composite-example`](https://github.com/LironHazan/actions-fe-guild-session/blob/master/.github/actions/composite-example/action.yml)
 and used by [`test_composite_action.yml`](https://github.com/LironHazan/actions-fe-guild-session/blob/master/.github/workflows/use_composite_action.yml)
 
-
-**Inline script example** 
-
+**Inline script example**
 
 ```yaml
-
-      - name: Greet
-        uses: actions/github-script@v6
-        with:
-          script: |
-            const prNumber = context.payload.pull_request.number;
-            const actor = context.actor;
-            const comment = `
-            👋 Hello @${actor}! 
-                What a nice code you contributed!
-            `;
-            await github.rest.issues.createComment({
-              owner: context.repo.owner,
-              repo: context.repo.repo,
-              issue_number: prNumber,
-              body: comment
-            });
+- name: Greet
+  uses: actions/github-script@v6
+  with:
+    script: |
+      const prNumber = context.payload.pull_request.number;
+      const actor = context.actor;
+      const comment = `
+      👋 Hello @${actor}! 
+          What a nice code you contributed!
+      `;
+      await github.rest.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: prNumber,
+        body: comment
+      });
 ```
 
-**Creating own action in typescript** 
+**Creating own action in typescript**
 
-Example of the custom action use  [`.github/workflow/use_custom_ts_action.yml`](https://github.com/LironHazan/actions-fe-guild-session/blob/master/.github/workflows/use_custom_ts_action.yml)
-
-
-
+Example of the custom action use [`.github/workflow/use_custom_ts_action.yml`](https://github.com/LironHazan/actions-fe-guild-session/blob/master/.github/workflows/use_custom_ts_action.yml)
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,58 @@
 import { getInput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 import { exec } from "@actions/exec";
-import {GitHub} from "@actions/github/lib/utils";
+import { GitHub } from "@actions/github/lib/utils";
 
 const ghToken = getInput("ghToken");
 
-(async () =>
-    await run()
-)()
+(async () => await run())();
 
 async function runCowsayCli(what: string): Promise<string> {
-    let output = '';
-    await exec('npx', ['cowsay', what], {
-        listeners: {
-            stdout: (data: Buffer) => {
-                output += data.toString();
-            }
-        }
-    });
-    return output;
+  let output = "";
+  await exec("npx", ["cowsay", what], {
+    listeners: {
+      stdout: (data: Buffer) => {
+        output += data.toString();
+      },
+    },
+  });
+  return output;
 }
-const COMMENT_ID = 'Your code is moognificant';
+const COMMENT_ID = "Your code is moognificant";
 
-async function getComment(octokit:  InstanceType<typeof GitHub>): Promise<any> {
-    // Check if a comment already exists for this PR from the bot
-    const commentsResult = await octokit.rest.issues.listComments({
-        repo: context.repo.repo,
-        owner: context.repo.owner,
-        issue_number: context.payload.pull_request?.number as number,
-    });
-    return commentsResult.data.find(comment => comment.body?.includes(COMMENT_ID));
+async function getComment(octokit: InstanceType<typeof GitHub>): Promise<any> {
+  // Check if a comment already exists for this PR from the bot
+  const commentsResult = await octokit.rest.issues.listComments({
+    repo: context.repo.repo,
+    owner: context.repo.owner,
+    issue_number: context.payload.pull_request?.number as number,
+  });
+  return commentsResult.data.find((comment) =>
+    comment.body?.includes(COMMENT_ID)
+  );
 }
 
 async function run() {
-    if (!ghToken || !context.payload.pull_request) return;
+  if (!ghToken || !context.payload.pull_request) return;
 
-    const octokit = getOctokit(ghToken)
-    console.log('Pr_number', context.payload.pull_request?.number)
-    const prevComment = await getComment(octokit)
+  const octokit = getOctokit(ghToken);
+  console.log("Pr_number", context.payload.pull_request?.number);
+  const prevComment = await getComment(octokit);
 
-    let output = await runCowsayCli(`${COMMENT_ID} ☀️`);
-    const commentBody = String(output).trim();
+  let output = await runCowsayCli(`${COMMENT_ID} ☀️`);
+  const commentBody = String(output).trim();
 
-    console.log(!!prevComment)
-    console.log(prevComment.id)
-
-    !!prevComment ? await octokit.rest.issues.updateComment({
+  !!prevComment
+    ? await octokit.rest.issues.updateComment({
         repo: context.repo.repo,
         owner: context.repo.owner,
         comment_id: prevComment.id,
-        body: commentBody
-    }) :
-        await octokit.rest.issues.createComment({
-            repo: context.repo.repo,
-            owner: context.repo.owner,
-            issue_number: context.payload.pull_request.number,
-            body: commentBody
-        })
+        body: commentBody,
+      })
+    : await octokit.rest.issues.createComment({
+        repo: context.repo.repo,
+        owner: context.repo.owner,
+        issue_number: context.payload.pull_request.number,
+        body: commentBody,
+      });
 }
